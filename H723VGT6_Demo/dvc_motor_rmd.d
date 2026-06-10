e410cd7de01dfe4d16357756bc67f0326c0a487b
Cjw <cjw2628232412@gmail.com>
2026-06-10 22:18:08 +0800

    6.10

--- H723VGT6_Demo/dvc_motor_rmd.d
+++ H723VGT6_Demo/dvc_motor_rmd.d
@@ -1,6 +1,7 @@
 h723vgt6_demo\dvc_motor_rmd.o: ..\User_dvc\dvc_motor_rmd.cpp
 h723vgt6_demo\dvc_motor_rmd.o: ..\User_dvc\dvc_motor_rmd.h
-h723vgt6_demo\dvc_motor_rmd.o: ../User_drv/drv_can.h
+h723vgt6_demo\dvc_motor_rmd.o: ../User_alg/alg_filter.h
+h723vgt6_demo\dvc_motor_rmd.o: ../User_drv/drv_math.h
 h723vgt6_demo\dvc_motor_rmd.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal.h
 h723vgt6_demo\dvc_motor_rmd.o: ../Core/Inc/stm32h7xx_hal_conf.h
 h723vgt6_demo\dvc_motor_rmd.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rcc.h
@@ -39,10 +40,9 @@ h723vgt6_demo\dvc_motor_rmd.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal
 h723vgt6_demo\dvc_motor_rmd.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_tim_ex.h
 h723vgt6_demo\dvc_motor_rmd.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart.h
 h723vgt6_demo\dvc_motor_rmd.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart_ex.h
-h723vgt6_demo\dvc_motor_rmd.o: ../User_alg/alg_filter.h
-h723vgt6_demo\dvc_motor_rmd.o: ../User_drv/drv_math.h
 h723vgt6_demo\dvc_motor_rmd.o: ../Middlewares/ST/ARM/DSP/Inc/arm_math.h
 h723vgt6_demo\dvc_motor_rmd.o: ../Drivers/CMSIS/Include/core_cm7.h
 h723vgt6_demo\dvc_motor_rmd.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
 h723vgt6_demo\dvc_motor_rmd.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\cfloat
 h723vgt6_demo\dvc_motor_rmd.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\float.h
+h723vgt6_demo\dvc_motor_rmd.o: ../User_drv/drv_can.h
